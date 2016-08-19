a0e02e3d245ba8980bc4b460f1c9f72d36e83f01
daph41 <daphinemoraa@gmail.com>
2016-08-19 20:10:24 +0300

    Add files via upload

--- test2.py
+++ test2.py
@@ -1,1 +1,27 @@
+import requests
+import csv
+from BeautifulSoup import BeautifulSoup
+
+url= 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
+response= requests.get(url)
+html= response. content
+
+soup= BeautifulSoup(html)
+table= soup.find('tbody', attrs= {'class':'stripe'})
+
+list_of_rows=[]
+for row in table.findAll('tr')[1:]:
+	list_of_cells= []
+	for cell in row.findAll('td'):
+		text= cell.text.replace('&nbsp;', ' ')
+		list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+outfile= open("/home/daph/Desktop/webscraping/test3.csv", "wb")
+writer= csv.writer(outfile)
+writer.writerow(["Last","First", "Middle", "Gender", "race", "Age", "City","State"])
+writer.writerows(list_of_rows)
+
+
+
 
